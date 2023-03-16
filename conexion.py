d98dbd1f5be4c62981a85d3a1b7a8dbdac0e365a
Ignacio <76023163+PIgnacioFranco@users.noreply.github.com>
2023-03-16 04:15:23 +0000

    Termino conexion.py y se creo cursor_del_pool.py

--- conexion.py
+++ conexion.py
@@ -1,4 +1,54 @@
+from logger_base import log
+from psycopg2 import pool
+import sys
+
 class Conexion:
     _BASEDEDATOS = 'tes-bd'
-    _USUARIOS =
-    pass
+    _USUARIO = 'posgres'
+    _CONTRASENA = '1234'
+    _HOST = '127.0.0.1'
+    _PUERTO = '3456'
+    _MIN = 1
+    _MAX = 5
+    _conexion = None
+    _pool = None
+
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
+            try:
+                cls._pool = pool.SimpleConnectionPool(
+                    cls._MIN, cls._MAX,
+                    database=cls._BASEDEDATOS,
+                    username=cls._USUARIO,
+                    password=cls._CONTRASENA,
+                    host=cls._HOST,
+                    port=cls._PUERTO
+                )
+                log.debug(f'Se obtuvo el pool: {cls._pool}')
+                return cls._pool
+            except Exception as e:
+                log.error(f'Error al obtener pool: {e}')
+                sys.exit()
+        else:
+            return cls._pool
+
+    @classmethod
+    def obtenerConexion(cls):
+        try:
+            cls._conexion = cls.obtenerPool().getconn()
+            log.debug(f'Se obtuvo conexion: {cls._conexion}')
+            return cls._conexion
+        except Exception as e:
+            log.error(f'Error al obtener conexion: {e}')
+            sys.exit()
+
+    @classmethod
+    def liberarConexion(cls, conexion):
+        cls.obtenerPool().putpool(conexion)
+        log.debug(f'Libero conexion al pool: {conexion}')
+
+    @classmethod
+    def cerrarConexion(cls):
+        cls.obtenerPool().closeall()
+        log.debug('Se cerro las conexiones')
---
+++ cursor_del_pool.py
@@ -0,0 +1,24 @@
+from logger_base import log
+from conexion import Conexion
+
+class CursorDelPool():
+    def __init__(self):
+        self._cursor = None
+        self._conexion = None
+
+    def __enter__(self):
+        log.debug('Ingreso al enter del context manager')
+        self._conexion = Conexion.obtenerConexion()
+        self._cursor = self._conexion.cursor()
+        return self._cursor
+
+    def __exit__(self, tipoExc, valorExc, detalleExc):
+        log.debug('Ingreso al exit del contxt manager')
+        if valorExc:
+            self._conexion.rollback()
+            log.error(f'Error transaccion, rollback: {valorExc} {tipoExc} {detalleExc}')
+        else:
+            self._conexion.commit()
+            log.debug('Se realizo commit de la transaccion')
+        self._cursor.close()
+        Conexion.liberarConexion(self._conexion)
