50129edc2e152ebb92e3f2034797f1d63014969f
Ignacio <76023163+PIgnacioFranco@users.noreply.github.com>
2023-03-17 18:21:31 +0000

    Se inicio el menú

---
+++ main.py
@@ -0,0 +1,21 @@
+# Menu pricipal
+print('Menu principal'.center(50,'-'))
+print('Opciones:')
+print('1 - Seleccionar:')
+print('2 - Insertar:')
+print('3 - Actualizar:')
+print('4 - Eliminar:')
+print('5 - Salir:')
+opcion = int(input('Ingrese opción: '))
+if opcion == 1:
+    pass
+elif opcion == 2:
+    pas
+elif opcion == 1:
+    pass
+elif opcion == 2:
+    pas
+elif opcion == 1:
+    pass
+else:
+    pass
